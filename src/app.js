1250950884a44daac6aa42751072d16fdbe09ccb
hczhang <hczhang@linux.com>
2023-10-09 10:59:45 +0800

    fix: dir error

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(helmet());
-app.use(favicon(path.join(__dirname, 'public', 'logo.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images/logo.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
